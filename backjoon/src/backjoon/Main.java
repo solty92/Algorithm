1de79c963107c944107c23d2f32426c6d83491ba
kimyc92 <kimyc92@gmail.com>
2021-02-01 21:46:28 +0900

    Main commit

--- backjoon/src/backjoon/Main.java
+++ backjoon/src/backjoon/Main.java
@@ -11,16 +11,31 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		String word = br.readLine();
+		int[][] apt = new int[15][15];
 		
+		for (int i = 0; i < apt.length; i++) {
+			apt[0][i] = i;
+			apt[i][1] = 1;
+		}
 		
-		for (int i = 0; i < word.length(); i++) {
-			word.charAt(i);
-			bw.write(word.charAt(i)+"\n");
+		for (int i = 1; i < apt.length; i++) {
+			for (int j = 2; j < apt.length; j++) {
+				apt[i][j] = apt[i][j-1] + apt[i-1][j];
+			}
+		}
+		
+		int testCase = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < testCase ; i++) {
+			int floor = Integer.parseInt(br.readLine());
+			int roomNumber = Integer.parseInt(br.readLine());
+			
+			bw.write("" + apt[floor][roomNumber] + "\n");
 		}
 		
 		bw.flush();
 		bw.close();
 		
 	}
+	
 }
