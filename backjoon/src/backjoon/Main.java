35728a0c70e2d9218f77c63516f6772992c3dc3a
kimyc92 <kimyc92@gmail.com>
2021-02-22 23:31:32 +0900

    Main commit

--- backjoon/src/backjoon/Main.java
+++ backjoon/src/backjoon/Main.java
@@ -6,42 +6,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		String array1 = "***";
-		String array2 = "* *";
+		int N = Integer.parseInt(br.readLine());
 		
-		int inpt = Integer.parseInt(br.readLine());
-		int times = inpt/3;
+		int[][] array = new int[N][2];
 		
-		if(inpt == 3) {
-			bw.write(array1 + "\n");
-			bw.write(array2 + "\n");
-			bw.write(array1);
-		}else {
-			for (int j = 0; j < times; j++) {
-				
-				for (int i = 0; i < times; i++) {
-					bw.write(array1);
-				}bw.newLine();
-				for (int i = 0; i < times; i++) {
-					bw.write(array2);
-				}bw.newLine();
-				for (int i = 0; i < times; i++) {
-					bw.write(array1);
-				}bw.newLine();
-			}
+		StringTokenizer st;
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			array[i][0] = Integer.parseInt(st.nextToken());
+			array[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
+		System.out.println(Arrays.deepToString(array));
 		
 		
 		
 		bw.close();
-		
 	}
 	
 }
