bd3fe298df3297593454ab7250059ecfa0765a4b
kimyc92 <kimyc92@gmail.com>
2021-03-12 23:35:34 +0900

    Main commit

--- backjoon/src/backjoon/Main.java
+++ backjoon/src/backjoon/Main.java
@@ -5,42 +5,68 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+	
+	static int edge;
+	static int[] startPoint = new int[2];
+	static int[] destination = new int[2];
+	static int[][] chessboard;
+	static boolean[][] isVisited;
+	static int[] rowArr = {-2, -1, 1, 2, 2, 1, -1, -2};
+	static int[] columnArr = {1, 2, 2, 1, -1, -2, -2, -1};
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		String array1 = "***";
-		String array2 = "* *";
+		int testCase = Integer.parseInt(br.readLine());
 		
-		int inpt = Integer.parseInt(br.readLine());
-		int times = inpt/3;
-		
-		if(inpt == 3) {
-			bw.write(array1 + "\n");
-			bw.write(array2 + "\n");
-			bw.write(array1);
-		}else {
-			for (int j = 0; j < times; j++) {
-				
-				for (int i = 0; i < times; i++) {
-					bw.write(array1);
-				}bw.newLine();
-				for (int i = 0; i < times; i++) {
-					bw.write(array2);
-				}bw.newLine();
-				for (int i = 0; i < times; i++) {
-					bw.write(array1);
-				}bw.newLine();
-			}
+		StringTokenizer st;
+		for (int i = 0; i < testCase; i++) {
+			edge = Integer.parseInt(br.readLine());
+			chessboard = new int[edge][edge];
+			
+			st = new StringTokenizer(br.readLine());
+			startPoint[0] = Integer.parseInt(st.nextToken());
+			startPoint[1] = Integer.parseInt(st.nextToken());
+			
+			st = new StringTokenizer(br.readLine());
+			destination[0] = Integer.parseInt(st.nextToken());
+			destination[1] = Integer.parseInt(st.nextToken());
+			
+			bfs();
 		}
 		
 		
-		
-		
 		bw.close();
+		
+	}
+	
+	public static void bfs() {
+		Queue<int[]> q = new LinkedList<int[]>();
+		isVisited[startPoint[0]][startPoint[1]] = true;
+		chessboard[startPoint[0]][startPoint[1]] = 0;
+		q.offer(new int[] {startPoint[0], startPoint[1]});
+		
+		while(!q.isEmpty()) {
+			int[] motherPoint = q.poll();
+			
+			for (int i = 0; i < 8; i++) {
+				int rowChildPoint = motherPoint[0] + rowArr[i];
+				int columnChildPoint = motherPoint[1] + columnArr[i];
+				
+				if(rowChildPoint >= 0 && columnChildPoint >= 0 && rowChildPoint < edge && columnChildPoint < edge && !isVisited[rowChildPoint][columnChildPoint]) {
+					isVisited[rowChildPoint][columnChildPoint] = true;
+					chessboard[rowChildPoint][columnChildPoint] = chessboard[motherPoint[0]][motherPoint[1]];
+					
+				}
+			}
+			
+		}
 		
 	}
 	
