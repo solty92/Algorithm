184327643388dee02dba7dfe948d6573ec22cb5a
YeaChan <kimyc92@gmail.com>
2021-03-23 22:24:55 +0900

    Solution commit (Q1204)

--- backjoon/src/swExpertAcademy/Solution.java
+++ backjoon/src/swExpertAcademy/Solution.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Solution {
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -13,5 +14,35 @@ public class Solution {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int testCase = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		int currentCase;
+		
+		for (int i = 0; i < testCase; i++) {
+			currentCase = Integer.parseInt(br.readLine());
+			int[] count = new int[101];
+			
+			st = new StringTokenizer(br.readLine());
+			
+			while(st.hasMoreTokens()) {
+				int score = Integer.parseInt(st.nextToken());
+				count[score]++;
+			}
+			
+			int maxApperance = 0;
+			int score = 0;
+			for (int j = 0; j < 101; j++) {
+				if(count[j] > maxApperance) {
+					maxApperance = count[j];
+					score = j;
+				}
+			}
+			
+			
+			bw.write("#" + currentCase + " " + score + "\n");
+		}
+		
+		
+		bw.close();
 	}
 }
