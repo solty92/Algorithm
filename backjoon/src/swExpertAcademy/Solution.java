a825a7956a5df19c469d01287e518627fe4ff7c8
YeaChan <kimyc92@gmail.com>
2021-03-23 20:55:01 +0900

    Solution commit test

--- backjoon/src/swExpertAcademy/Solution.java
+++ backjoon/src/swExpertAcademy/Solution.java
@@ -2,15 +2,16 @@ package swExpertAcademy;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Solution {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		
+		int testCase = Integer.parseInt(br.readLine());
 	}
 }
