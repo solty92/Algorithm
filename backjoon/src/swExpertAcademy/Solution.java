0aa6c73b3b8345a239eace09a49af039320b0626
YeaChan <kimyc92@gmail.com>
2021-03-23 20:52:09 +0900

    Solution commit test

--- backjoon/src/swExpertAcademy/Solution.java
+++ backjoon/src/swExpertAcademy/Solution.java
@@ -1,5 +1,16 @@
 package swExpertAcademy;
 
-public class Solution {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
+public class Solution {
+	public static void main(String[] args) {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+	}
 }
