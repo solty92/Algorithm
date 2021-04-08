2ed9cc0b797e2bdf65ed6761a52d783c62fe6fe8
YeaChan <kimyc92@gmail.com>
2021-04-08 23:33:21 +0900

    Solution commit (SW1258)

--- backjoon/src/swExpertAcademy/Solution.java
+++ backjoon/src/swExpertAcademy/Solution.java
@@ -6,87 +6,47 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Iterator;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Solution {
-
-	static int[][] ladderArr;
-	static int shortestStartPoint;
-	static boolean[][] isVisited;
-	static int tmpCount;
-	static int minCount;
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		static int[][] storage;
+		static boolean[][] isVisited;
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+		
+		int caseCount = Integer.parseInt(br.readLine());
+		
 		StringTokenizer st;
-
-		for (int testCase = 1; testCase <= 10; testCase++) {
-			br.readLine();
-
-			ladderArr = new int[100][100];
-			isVisited = new boolean[100][100];
-			minCount = Integer.MAX_VALUE;
-			shortestStartPoint = 0;
-
-			// 지도 그리기
-			for (int row = 0; row < 100; row++) {
-
+		
+		for (int testCase = 1; testCase <= caseCount; testCase++) {
+			
+			int N = Integer.parseInt(br.readLine());
+			
+			storage = new int[N][N];
+			
+			// 창고 담기 
+			for (int row = 0; row < N; row++) {
 				st = new StringTokenizer(br.readLine());
 				
-				for (int column = 0; column < 100; column++) {
-					ladderArr[row][column] = Integer.parseInt(st.nextToken());
+				for (int column = 0; column < N; column++) {
+					storage[row][column] = Integer.parseInt(st.nextToken());
 				}
 			}
 			
-//			System.out.println(Arrays.deepToString(ladderArr));
-			
-			// 끝점에서 시작점으로 가기
-			for (int endPoint = 0; endPoint < 100; endPoint++) {
-				tmpCount = 0;
-				if(ladderArr[99][endPoint] == 1) dfs(99, endPoint);
+			// 화학 물질 담김 용기 찾기 (0이 아닌거)
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					
+				}
 			}
 			
-			bw.write("#" + testCase + " " + shortestStartPoint + "\n");
-			System.out.println("---------------------------------------------");
-			
-
 		}
-
+		
+		
 		bw.close();
 	}
-
-	public static void dfs(int startRow, int startColumn) {
-
-		isVisited[startRow][startColumn] = true;
-		tmpCount++;
-
-		if (startRow == 0) {
-			if (tmpCount < minCount) {
-				System.out.println("count : " + tmpCount + "   <   " + "maxCount : " + minCount);
-				System.out.println("shortestStartPoint : " + shortestStartPoint);
-				minCount = tmpCount;
-				shortestStartPoint = startColumn;
-			}
-			return;
-		}
-
-		int left = startColumn - 1;
-		int right = startColumn + 1;
-		int up = startRow - 1;
-
-		if (left >= 0 && ladderArr[startRow][left] == 1 && !isVisited[startRow][left]) {
-			dfs(startRow, left);
-
-		} else if (right < 100 && ladderArr[startRow][right] == 1 && !isVisited[startRow][right]) {
-			dfs(startRow, right);
-		} else {
-			dfs(up, startColumn);
-		}
-
-	}
-
 }
