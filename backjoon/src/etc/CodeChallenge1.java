f88c88e357d0613639fa2250cdc8de48b1fe1ee4
YeaChan <kimyc92@gmail.com>
2021-04-15 22:53:33 +0900

    CodeChallenge1, 2 commit

---
+++ backjoon/src/etc/CodeChallenge1.java
@@ -0,0 +1,22 @@
+package etc;
+
+import java.util.Stack;
+
+public class CodeChallenge1 {
+	
+	public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for(int i = 0 ; i < absolutes.length ; i++) {
+            if(signs[i]) answer += absolutes[i];
+            else answer -= absolutes[i];
+        }
+        
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		
+		
+	}
+}
---
+++ backjoon/src/etc/CodeChallenge2.java
@@ -0,0 +1,48 @@
+package etc;
+
+import java.util.Stack;
+
+public class CodeChallenge2 {
+	
+	public static int solution(String s) {
+        StringBuffer sb = new StringBuffer(s);
+        Stack<Character> stack = new Stack<Character>();
+        
+        int answer = 0;
+        
+        for(int j = 0 ; j < s.length(); j++) {
+        	
+        	stack.clear();
+        	stack.push('*');
+         
+            for (int i = 0; i < s.length(); i++) {
+                char tmp = sb.charAt(i);
+                
+                if(tmp == '(' || tmp == '{' || tmp == '[') stack.push(tmp);
+                else if(tmp == ')' && stack.peek() == '(') stack.pop();
+                else if(tmp == '}' && stack.peek() == '{') stack.pop();
+                else if(tmp == ']' && stack.peek() == '[') stack.pop();
+                else {
+                	stack.clear();
+                	break;
+                }
+                
+            }
+            
+            if(stack.size() == 1) answer++;
+            
+            char ttmp = sb.charAt(0);
+            sb.deleteCharAt(0);
+            sb.append(ttmp);
+            
+        }
+        
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		String st = "[](){}"	;
+		System.out.println(solution(st));
+		
+	}
+}
