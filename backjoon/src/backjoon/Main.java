cea6575bbfb91155c13d2fc228b4bfac6a321162
YeaChan <kimyc92@gmail.com>
2021-04-25 00:58:30 +0900

    Main commit (Q2042)

--- backjoon/src/backjoon/Main.java
+++ backjoon/src/backjoon/Main.java
@@ -5,52 +5,154 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	
-	static BufferedWriter bw;
 	static int[] arr;
-	static boolean[] isUsed;
-	
-	static int N, M;
+	static int lvl;
+	static BufferedWriter bw;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
 		
-		arr = new int[M+1];
-		isUsed = new boolean[N+1];
+		lvl = 0;
+		while(N > Math.pow(2, lvl)) {
+			
+			lvl++;
+			
+		}
 		
-		permutation(1);
+		int childeNodes = (int) Math.pow(2, lvl);
+		arr = new int[childeNodes * 2];
+		
+		for (int i = childeNodes; i < childeNodes + N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+//		System.out.println(Arrays.toString(arr));
 		
 		
-	
+		
+		// 조상 노드 채우기
+		int tmpLvl = lvl;
+		while(tmpLvl > 0) {
+			
+			for (int i = (int) Math.pow(2, tmpLvl); i < (int) Math.pow(2, tmpLvl) * 2; i += 2) {
+				
+				arr[i/2] = arr[i] + arr[i+1];
+				
+			}
+			
+			tmpLvl--;
+		}
+		
+//		System.out.println(Arrays.toString(arr));
+		
+		for (int i = 0; i < M+K; i++) {
+			
+			st = new StringTokenizer(br.readLine());
+			
+			int which = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			if(which == 1) change(a - 1 + childeNodes, b);
+			else sum(a - 1 + childeNodes, b - 1 + childeNodes);
+			
+		}
+		
+//		System.out.println(Arrays.toString(arr));
+		
+		
 		bw.close();
-
 	}
 	
-	private static void permutation(int lvl) throws IOException {
+	private static void change(int changeThis, int toThis) {
 		
-		if(lvl > M) {
+		int difference = toThis - arr[changeThis];
+		arr[changeThis] = toThis;
+		
+		int tmpLvl = lvl;
+		
+		while(tmpLvl > 0) {
 			
-			for(int a : arr) {
-				bw.write(a + " ");
+			if(changeThis % 2 == 0) {
+				changeThis /= 2;
+				arr[changeThis] += difference;
+			}else {
+				changeThis -= 1;
+				changeThis /= 2;
+				arr[changeThis] += difference;
 			}
-			bw.write("\n");
 			
-			return;
+			tmpLvl--;
 			
 		}
 		
-		for (int i = 1; i < arr.length; i++) {
-			
+	}
+	
+	private static void sum(int from, int to) throws IOException {
+		
+		int result = 0;
+		
+		if(from == to) {
+			bw.write(arr[from] + "\n");
+			return;
 		}
+		
+		if(from % 2 != 0) {
+			result += arr[from];
+			from++;
+		}
+		
+		if(to % 2 == 0) {
+			result += arr[to];
+			to--;
+		}
+		
+		int leftIdx = from;
+		int rightIdx = to;
+		
+		while(leftIdx < rightIdx) {
+			
+			if(leftIdx % 2 == 0) {
+				leftIdx /= 2;
+			}else {
+				result += arr[leftIdx];
+				leftIdx++;
+				
+				leftIdx /= 2;
+			}
+			
+			if(rightIdx % 2 != 0) {
+				rightIdx--;
+				rightIdx /= 2;
+			}else {
+				result += arr[rightIdx];
+				rightIdx -= 2;
+				
+				rightIdx /= 2;
+			}
+		}
+		
+		if(leftIdx == rightIdx) {
+			result += arr[leftIdx];
+			bw.write(result + "\n");
+		}else {
+			bw.write(result + "\n");
+		}
+		
+		
 		
 	}
 }
