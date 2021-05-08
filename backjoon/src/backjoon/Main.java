075cf3de61b415516f635088fb736728f3ef3a83
YeaChan <kimyc92@gmail.com>
2021-05-08 23:58:46 +0900

    Main commit (Q13904)

--- backjoon/src/backjoon/Main.java
+++ backjoon/src/backjoon/Main.java
@@ -5,57 +5,31 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int[] arr;
-	static int N;
-	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 		
-		arr = new int[N+1];
+		StringTokenizer st;
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[][] arr = new int[N][2];
 		
-		for (int i = 1; i <= N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
-		int result = price();
+		bw.write(Arrays.deepToString(arr));
 		
-		bw.write(result + "");
 		
 		bw.close();
-	}
-
-	private static int price() {
-		
-		// 가장 비싼 카드팩 찾기 
-		int mostExpensiveIdx = 0;
-		double perPrice = 0;
-		for (int i = 1; i <= N; i++) {
-			
-			if((double) arr[i]/i > perPrice) {
-				perPrice = (double) arr[i]/i ;
-				mostExpensiveIdx = i;
-			}
-			
-		}
-		
-		if(N % mostExpensiveIdx == 0) {
-			return ( N / mostExpensiveIdx) * arr[mostExpensiveIdx];
-		}else {
-			int tmp = ( N / mostExpensiveIdx) * arr[mostExpensiveIdx];
-			tmp += arr[( N % mostExpensiveIdx )] * ( N % mostExpensiveIdx );
-			
-			return tmp;
-		}
-		
 	}
 }
