a5462945bb41f329559e996a67d52ea4559e00ab
YeaChan <kimyc92@gmail.com>
2021-05-13 22:36:34 +0900

    CodeChallenge5 commit

---
+++ backjoon/src/etc/CodeChallenge5.java
@@ -0,0 +1,35 @@
+package etc;
+
+class CodeChallenge5 {
+    public int solution(int left, int right) {
+        int answer = 0;
+        
+        for (int i = left; i <= right; i++) {
+			
+        	int count = divisorCounter(i);
+        	
+        	if(count % 2 == 0) {
+        		answer += i;
+        	}else {
+        		answer -= i;
+        	}
+        	
+		}
+        System.out.println(answer);
+        return answer;
+    }
+    
+    public int divisorCounter(int number) {
+    	int count = 0;
+    	
+    	for (int i = number; i > 1; i--) {
+    		
+			if(number % i == 0) {
+				count++;
+			}
+		}
+    	
+    	return count + 1;
+    }
+    
+}
