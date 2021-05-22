8f098279df6298ec8ff6fa121ad60c67afa3c1d0
YeaChan <kimyc92@gmail.com>
2021-05-22 16:31:13 +0900

    Q2163 commit

---
+++ backjoon/src/backjoon/Q2163.java
@@ -0,0 +1,30 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Q2163 {
+	
+	static int[][] combiArr = new int[30][30];
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int result = N * M - 1;
+		
+		bw.write(result + "");
+		
+		bw.close();
+	}
+}
